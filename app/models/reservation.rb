2773bfe99fde031db0abc3f68c25b8510fee324a
Reid <cr00657@heron136.eps.surrey.ac.uk>
2020-12-12 18:28:23 +0000

    css and application.html

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
+  validates :Name, presence:true
+  validates :StartTime, presence:true
+  validates :EndTime, presence:true
+
   belongs_to :user
   belongs_to :employee
 end
